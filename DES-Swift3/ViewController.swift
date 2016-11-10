ca22f972e5c04d1f24bbc90c9b21fbd8a26ad2a7
Nguyen Van Thieu B <nguyen.van.thieub@framgias-Mac-mini-6.local>
2016-11-10 15:34:06 +0700

    continue

--- DES-Swift3/ViewController.swift
+++ DES-Swift3/ViewController.swift
@@ -13,12 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Start Sinh Khoa
-        convertKeyToHex()
+        let key_string = "thieumao"
+        let key_hex = convertKeyToHex(string: key_string)
+        let key_bin = DESConvert.hexToBin(hex: key_hex)
+        print(key_bin)
     }
     
-    func convertKeyToHex() {
-        var hex = DESConvert.stringToHex();
-        print("hex ", hex)
+    func convertKeyToHex(string: String) -> String {
+        var hex = DESConvert.stringToHex(string: string);
+        if (hex.characters.count>=16) {
+            let start = hex.index(hex.startIndex, offsetBy: 0)
+            let end = hex.index(hex.startIndex, offsetBy: 16)
+            let range = start..<end
+            hex = hex.substring(with: range)
+        } else {
+            for _ in 0..<16-hex.characters.count {
+                hex += "0"
+            }
+        }
+        return hex
     }
     
 }
